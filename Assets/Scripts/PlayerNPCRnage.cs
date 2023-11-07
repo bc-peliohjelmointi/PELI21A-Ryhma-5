263b1b4ccb1caf190c0413a7583bfe66f39f346c
LeeviSuominen <115700342+LeeviSuominen@users.noreply.github.com>
2023-11-07 13:32:33 +0200

    Revert "npc interactable ui scripts"
    This reverts commit 29d260bf4db15edcbf55dfae0c9592e5415dbd2a.

--- Assets/Scripts/PlayerNPCRnage.cs
+++ Assets/Scripts/PlayerNPCRnage.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerInteract : MonoBehaviour
+public class Friend : MonoBehaviour
 {
     private void Update()
     {
@@ -18,20 +18,5 @@ public class PlayerInteract : MonoBehaviour
                 }
             }
         }
-    }
-
-    public FriendInteractable GetInteractableObject()
-    {
-        float interactRange = 2f;
-        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
-        foreach (Collider collider in colliderArray)
-        {
-            if (collider.TryGetComponent(out FriendInteractable friendInteractable))
-            {
-                return friendInteractable;
-
-            }
-        }
-        return null;
     }
 }
