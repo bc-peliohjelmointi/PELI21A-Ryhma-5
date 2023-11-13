dd6a8b5eed96169bf8f57d81214c18f0daa6d950
RuikkaAnton <115700078+RuikkaAnton@users.noreply.github.com>
2023-11-13 10:44:28 +0200

    npc ohjelmointi valmis

--- Assets/Scripts/FriendInteractable.cs
+++ Assets/Scripts/FriendInteractable.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FriendInteractable : MonoBehaviour
 {
-    public void Interact()
+    public Transform player;
+   public void Interact()
     {
+        transform.LookAt(player);
         Debug.Log("Interact!");
     }
 }
