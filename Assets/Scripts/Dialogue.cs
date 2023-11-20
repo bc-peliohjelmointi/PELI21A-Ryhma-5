81185016dfe1d6556dba7d83c73246bab70ccfa5
LeeviSuominen <115700342+LeeviSuominen@users.noreply.github.com>
2023-11-20 12:52:38 +0200

    Dialogue started
    Player movement stops when starting dialogue and doesn't come back. I'll fix it tomorrow.

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -2,19 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using StarterAssets;
+
 public class Dialogue : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI textComponent; 
     [SerializeField] float textSpeed; 
     [SerializeField] string[] lines;
 
+    public bool dialogueActive;
+
     private int index;
 
     void StartDialogue(){
+        dialogueActive= true;
         index = 0;
+        StartCoroutine(TypeLine());
     } 
 
-    //IENumerator TypeLine(){
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
 
-   // }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(textComponent.text == lines[index])
+            {
+                NextLine();
+            }
+
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[index];  
+            }
+        }
+    }
+    IEnumerator TypeLine(){
+        foreach (char c in lines[index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+   }
+
+    void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            textComponent.text = string.Empty;
+            StartCoroutine (TypeLine());
+        }
+
+        else
+        {
+            gameObject.SetActive(false);
+            dialogueActive= false;
+        }
+    }
+   
 }
--- Assets/Scripts/PlayerInteract.cs
+++ Assets/Scripts/PlayerInteract.cs
@@ -1,13 +1,28 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerInteract : MonoBehaviour
 {
+    [SerializeField] GameObject dialogueBox;
+    private FirstPersonController FPSInput;
+
+    private void Start()
+    {
+        dialogueBox.SetActive(false);
+        FPSInput = GetComponent<FirstPersonController>();
+    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            dialogueBox.SetActive(true);
+            FPSInput.MoveSpeed= 0;
+            FPSInput.RotationSpeed= 0;
+            FPSInput.SprintSpeed= 0;
+
             float interactRange = 2f;
             Collider [] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
             foreach (Collider collider in colliderArray)
