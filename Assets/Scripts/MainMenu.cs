47eb4400e9b987690c148e9dd09bca8ae9ba31c5
RuikkaAnton <115700078+RuikkaAnton@users.noreply.github.com>
2023-12-13 14:01:09 +0200

    story screen done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class PlayButton : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(4);
     }
 
     public void QuitGame()
